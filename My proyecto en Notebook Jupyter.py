abffbc2bb6e5e8ae1298663967f6609bee24c44e
Tatiana Rincon <114424783+Tcrincon22@users.noreply.github.com>
2022-10-04 21:34:04 -0500

    Add files via upload

---
+++ My proyecto en Notebook Jupyter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> My Jupyter Notebook on IBM Watson Studio
+
+# <H2> Tatiana Camila Rincon Martinez
+
+# Ingeniera Industrial 
+
+# Estoy interesada en la ciencia de datos porque en un rama que permite estudiar ademas de lenguajes de programacion, matematicas, estadistica, ayuda a mantenernos actualizados con el mundo y la ciencia 
+
+# <H3> El siguiente codigo debe dar la multiplicacion de dos numeros pares
+
+# In[1]:
+
+
+4*4
+
+
+# *Hola*
+# _Hola_ 
+# [I'm an inline-style link](https://www.google.com/maps/?hl=es)
+# 1. Azul
+# 2. Rojo
+# - Amarillo
+# 
+# 
+# 
+# 
+# 
